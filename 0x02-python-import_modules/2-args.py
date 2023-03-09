e631fd29b184f9ce233d57bb441c661c8974c688
Okafor Nelson <okafornel@gmail.com>
2023-03-09 07:22:20 -0800

    Create task 2 file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the list of arguments for the program"""
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+elif len(argv) > 1:
+    if len(argv) == 2:
+        print("1 argument:")
+        print("{}: {}".format(1, argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))

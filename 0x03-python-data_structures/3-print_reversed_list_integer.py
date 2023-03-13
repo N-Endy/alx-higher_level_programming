cbbfff6ded9b182b005c73d4c97075e0690f2b40
Okafor Nelson <okafornel@gmail.com>
2023-03-13 04:35:11 -0700

    Revert commit to reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+<<<<<<< HEAD
     if my_list
         for num in reversed(my_list):
             print("{:d}".format(num))
+=======
+    for num in my_list.reverse():
+        print("{:d}".format(num))
+>>>>>>> parent of f6f6610... Change reverse to reversed

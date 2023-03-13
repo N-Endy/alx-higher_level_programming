4db30e751e79cd508c325aec671b9fdaebf5a6b3
Okafor Nelson <okafornel@gmail.com>
2023-03-13 02:59:04 -0700

    Add "return None" conditions

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0:
-        if idx < len(my_list):
+    if idx < 0:
+        return None
+        if idx > len(my_list) - 1:
+            return None
+        else:
             print("Element at index {:d} is {}".format(idx, my_list[idx]))

9b8286163835a4b6e5e7d852ce016e70a5832834
Okafor Nelson <okafornel@gmail.com>
2023-03-29 15:52:20 -0700

    Create task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Define a Square class"""
+
+
+class Square:
+    """Blueprint for a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+            size (int): The length of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+    @property
+    def area(self):
+        """Return the area of the square"""
+        return self.size ** 2

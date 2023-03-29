d0073f750e3b38878f84cc90533d70a329479035
Okafor Nelson <okafornel@gmail.com>
2023-03-29 16:38:17 -0700

    Add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Define a Square class"""
+
+
+class Square:
+    """Blueprint for a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+            size (int): The length of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size/length of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square represented with # sign"""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
